0b8008ff80ef7800ed24e5ffce1710334233929c
Chih Chiu <chih.chiu.19@gmail.com>
2017-01-09 13:00:12 -0800

    initial commitment

---
+++ Assets/GuiLogger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuiLogger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onGui() {
+		GUILayout.BeginArea(new Rect(10, 10, 100, 100));
+		GUILayout.Button("Click me");
+		GUILayout.Button("Or me");
+		GUILayout.EndArea();
+
+		GUILayout.Label("\n\n C O M P A S S");
+		GUILayout.Label("Input.compass.enabled <<<===>>> " + Input.
+			compass.enabled.ToString());
+		GUILayout.Label("Input.compass.headingAccuracy <<<===>>> " +
+			Input.compass.headingAccuracy.ToString());
+		GUILayout.Label("Input.compass.magneticHeading <<<===>>> " +
+			Input.compass.magneticHeading.ToString());
+		GUILayout.Label("Input.compass.rawVector <<<===>>> " + Input.
+			compass.rawVector.ToString());
+		GUILayout.Label("Input.compass.timestamp <<<===>>> " + Input.
+			compass.timestamp.ToString());
+		GUILayout.Label("Input.compass.trueHeading <<<===>>> " + Input.
+			compass.trueHeading.ToString());
+	}
+}
